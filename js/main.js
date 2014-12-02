df77f7e15020f539a50e1c43f8b8e7b19f1eb7d8
Benjamin Chelli <benjamin@chelli.net>
2014-12-01 20:06:35 -0500

    Finish board interaction, TODO: AI

--- js/main.js
+++ js/main.js
@@ -4,17 +4,30 @@
 
   "use strict";
 
+  /*
+   * CONSTANTS
+   */
   var CELL_EMPTY    = 0;
   var CELL_PLAYER1  = 1;
   var CELL_PLAYER2  = 2;
 
-  var cellTypesMapping = {};
-  cellTypesMapping[CELL_EMPTY]   = '';
-  cellTypesMapping[CELL_PLAYER1] = 'cross';
-  cellTypesMapping[CELL_PLAYER2] = 'circle';
+  var PLAYED_OK                 = 3;
+  var PLAYED_NOK_GAME_OVER      = 4;
+  var PLAYED_NOK_CELL_NOT_EMPTY = 5;
 
+  var CELL_MAPPING = {};
+  CELL_MAPPING[CELL_EMPTY]   = '';
+  CELL_MAPPING[CELL_PLAYER1] = 'cross';
+  CELL_MAPPING[CELL_PLAYER2] = 'circle';
+
+
+
+
+  /*
+   * VARIABLES
+   */
   var board;
-  var gameOver = true;
+  var winner;
 
 
 
@@ -42,6 +55,7 @@
 
 
 
+
   /*
    * PROGRAM LOOP
    */
@@ -55,7 +69,7 @@
         break;
 
       case 'play':
-        if(play(action.cell, CELL_PLAYER1)){
+        if(PLAYED_OK == play(action.cell, CELL_PLAYER1)){
           playAI();
         }
         break;
@@ -69,43 +83,114 @@
 
 
 
+
   /*
    * HELPERS
    */
+
+
   function initBoard(){
     board = [
       [CELL_EMPTY, CELL_EMPTY, CELL_EMPTY],
       [CELL_EMPTY, CELL_EMPTY, CELL_EMPTY],
       [CELL_EMPTY, CELL_EMPTY, CELL_EMPTY]
     ];
-    gameOver = false;
+    winner = CELL_EMPTY;
+    if($('#player-starting').val()=='ai'){
+      playAI();
+    }
   }
-  function play(cell, player){
-    if(gameOver) return;
+
+
+  function getCoords(cell){
     var $el = $(cell);
     var x = parseInt($el.attr('cell-x'), 10)-1;
     var y = parseInt($el.attr('cell-y'), 10)-1;
-    if(board[y][x] == CELL_EMPTY){
-      board[y][x] = player;
-      return true;
-    }
-    return false;
+
+    return {x:x,y:y};
   }
+
+
+  function play(cell, player){
+
+    if(isGameOver()) return PLAYED_NOK_GAME_OVER;
+
+    var coords = getCoords(cell);
+
+    if(board[coords.y][coords.x] == CELL_EMPTY){
+      board[coords.y][coords.x] = player;
+      checkWinner();
+      return PLAYED_OK;
+    }
+
+    return PLAYED_NOK_CELL_NOT_EMPTY;
+  }
+
+
   function draw(){
+
+    // the board
     $('#board .cell').each(function(index, el){
       var $el = $(el);
-      var x = parseInt($el.attr('cell-x'), 10)-1;
-      var y = parseInt($el.attr('cell-y'), 10)-1;
-      $el.attr('cell-type',cellTypesMapping[board[y][x]]);
+      var coords = getCoords($el);
+
+      $el.attr('cell-type',CELL_MAPPING[board[coords.y][coords.x]]);
     });
+
+    // the winner
+    if(isGameOver()){
+      $('#winner').removeClass('hidden').find('.cell').attr('cell-type', CELL_MAPPING[winner]);
+    } else {
+      $('#winner').addClass('hidden');
+    }
+
   }
+
+
   function playAI(){
+    // dummy AI
     var needToPlay = true;
     $('#board .cell').each(function(index, el){
-      if(needToPlay && play(el, CELL_PLAYER2)){
+      if(needToPlay && PLAYED_OK == play(el, CELL_PLAYER2)){
         needToPlay = false;
       }
     });
+  }
+
+
+  function isGameOver(){
+    return winner!=CELL_EMPTY;
+  }
+
+
+  function checkWinner(){
+
+    for(var i=0;i<3;i++){
+      // check lines
+      if(board[i][0] != CELL_EMPTY && board[i][0] == board[i][1] && board[i][0] == board[i][2]){
+        winner = board[i][0];
+        return true;
+      }
+
+      // check cols
+      if(board[0][i] != CELL_EMPTY && board[0][i] == board[1][i] && board[0][i] == board[2][i]){
+        winner = board[0][i];
+        return true;
+      }
+    }
+
+    // check diag 1
+    if(board[0][0] != CELL_EMPTY && board[0][0] == board[1][1] && board[0][0] == board[2][2]){
+      winner = board[0][0];
+      return true;
+    }
+    // check diag 2
+    if(board[0][2] != CELL_EMPTY && board[0][2] == board[1][1] && board[0][2] == board[2][0]){
+      winner = board[0][2];
+      return true;
+    }
+
+    return false;
   }
 
 
